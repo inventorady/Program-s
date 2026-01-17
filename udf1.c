1e79c1bacd66aa38be06c2ea603b7cbcf2119a6f
Aditya Vyas <iam.vyasaditya@gmail.com>
2026-01-17 19:00:16 +0530

    Add files via upload

---
+++ udf1.c
@@ -0,0 +1,35 @@
+/*
+title:-Write a C program to define and call a user defined function to find the sum of two integers
+name:-hardik kanani
+*/
+#include<stdio.h>
+#include<stdlib.h>
+
+int addtwo(int a,int b);
+
+int main()
+{
+    int a,b,sum;
+    system("cls");
+
+    printf("\n=====================================");
+
+    printf("\n Enter the two number: ");
+    scanf("%d %d",&a,&b);
+    sum=addtwo(a,b);
+    printf("\n Sum of two number= %d",sum);
+
+}
+
+int addtwo(int a,int b)
+{
+    int result;
+    result=a+b;
+    return result;
+
+    printf("\n=====================================");
+ 
+}
+
+
+
