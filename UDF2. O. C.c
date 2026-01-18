46d7e6470a74298dd3a1786378f12807160f48e0
Aditya Vyas <iam.vyasaditya@gmail.com>
2026-01-18 10:01:53 +0530

    Add files via upload

---
+++ UDF2. O. C.c
@@ -0,0 +1,21 @@
+/*
+title:-Write a C program to demonstrate a user defined function with no arguments and no return value to display a welcome message.
+name:-Ady
+*/
+#include<stdio.h>
+#include<stdlib.h>
+
+void good();
+
+int main()
+{
+      system("cls");
+     good();
+    return 0;
+}
+
+void good()
+{
+    printf("welcome to amroli sfi colleges");
+}
+
