1949d8bcab9ede4617aa039f29c159bf0c4587fa
Aditya Vyas <iam.vyasaditya@gmail.com>
2026-01-30 22:27:09 +0530

    Add files via upload

---
+++ pattern with loop.c
@@ -0,0 +1,27 @@
+
+/*  
+TITLE:WAP TO make PATTERN using loop
+       3  3  3
+       2  2
+       1
+*/
+ DEVLOPED BY: adi
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+	int i,j,k;
+		system("cls");
+		for(i=5;i>=1;i--)
+		{
+			for(j=1;j<=i;j++)
+			{
+				printf("%d",i);
+						
+			}
+			
+			printf("\n");
+		}
+		return 0;
+}
